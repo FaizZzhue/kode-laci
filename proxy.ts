8a78658543ac3a331580786491870fefdcf1c0f6
FaizZzhue <fynyourbaeid18@gmail.com>
2026-05-25 19:11:23 +0700

    feat: proxy add Next

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function Proxy(request: NextRequest) {
+export function proxy(request: NextRequest) {
     return NextResponse.next();
 }
